7fa77bcb9e4cb0f35c633886aa3ae532f65d709c
AshishAanand <162909294+AshishAanand@users.noreply.github.com>
2025-12-25 14:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (69.26%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        std::sort(nums.begin(), nums.end());
         int expectedSum = n*(n+1)/2;
         int actualSum = 0;
         for(int i = 0; i < n; i++){
